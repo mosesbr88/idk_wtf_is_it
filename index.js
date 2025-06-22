3b4f3c9b8dd8cb5b411ab7496e8606300c884f9f
mosesbr88 <mosesbr88@hotmail.com>
2025-06-22 20:32:44 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+//const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+
+//const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+app.post('/webhook', (req, res) => {
+  console.log('Received:', JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
