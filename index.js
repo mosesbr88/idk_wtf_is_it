ccc308a8c6c54e674191d2cb1a318ca8e0177b03
mosesbr88 <mosesbr88@hotmail.com>
2025-06-22 20:37:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ app.use(express.json());
 
 //const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
+app.get("/webhook", (req, res) => {
+  console.log("tex");
+  res.send(true);
+});
+
+
 app.post('/webhook', (req, res) => {
   console.log('Received:', JSON.stringify(req.body, null, 2));
   res.sendStatus(200);
