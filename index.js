5d5e209f93cd97a4123c32b0272471d1c1ebb7fa
mosesbr88 <mosesbr88@hotmail.com>
2025-06-22 20:43:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ app.use(express.json());
 
 //const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
+// Webhook verification
+app.get('/webhook', (req, res) => {
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token === VERIFY_TOKEN) {
+    console.log('WEBHOOK_VERIFIED');
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+
 app.get("/webhook", (req, res) => {
   console.log("tex");
   res.send(true);
